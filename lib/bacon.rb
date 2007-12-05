5be4ac6a0b874dafd3aba6e14055846b448b14f4
Christian Neukirchen <chneukirchen@gmail.com>
2007-12-05 14:41:26 +0100

    Add copyright
    darcs-hash:20071205134126-4fc50-7ef1cabdc3e28be8c0d93a0583c89a81b2996885.gz

--- lib/bacon.rb
+++ lib/bacon.rb
@@ -1,5 +1,11 @@
 # Bacon -- small RSpec clone.
+#
 # "Truth will sooner come out from error than from confusion." ---Francis Bacon
+
+# Copyright (C) 2007 Christian Neukirchen <purl.org/net/chneukirchen>
+#
+# Bacon is freely distributable under the terms of an MIT-style license.
+# See COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 module Bacon
   VERSION = "0.2"
