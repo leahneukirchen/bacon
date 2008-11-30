b473f201b54ebbe5c8cd1a59ee8571d74bf2a7ed
Christian Neukirchen <chneukirchen@gmail.com>
2008-11-30 11:00:28 +0100

    Add Knock output

--- lib/bacon.rb
+++ lib/bacon.rb
@@ -97,6 +97,23 @@ module Bacon
     end
   end
 
+  module KnockOutput
+    def handle_specification(name)  yield  end
+
+    def handle_requirement(description)
+      ErrorLog.replace ""
+      error = yield
+      if error.empty?
+        puts "ok - %s" % [description]
+      else
+        puts "not ok - %s: %s" % [description, error]
+        puts ErrorLog.strip.gsub(/^/, '# ')  if Backtraces
+      end
+    end
+
+    def handle_summary;  end
+  end
+
   extend SpecDoxOutput          # default
 
   class Error < RuntimeError
